baf9ad2d9e7ba0043b99ca3691a55db54151a80a
Alex Richardson <arichardson110@academic.rrc.ca>
2019-11-05 23:41:55 -0600

    added active_storage for image upload capabilities for admin users, the index dashboard was reconfiguered to naivly display the images on file

--- app/admin/items.rb
+++ app/admin/items.rb
@@ -1,3 +1,31 @@
+# frozen_string_literal: true
+
 ActiveAdmin.register Item do
-  permit_params :name, :price, :description, :image, :sprit, :category_id
+  permit_params :name, :price, :description, :image, :sprit, :category_id, :upload
+
+  form do |f|
+    f.semantic_errors
+    f.inputs
+    f.inputs do
+      f.input :upload, as: :file
+    end
+    f.actions
+  end
+
+  index do
+    selectable_column
+    column :name
+    column :price
+    column :description
+    column :image do |item|
+      image_tag item.image, alt: "#{item.name}_image"
+    end
+    column :sprit do |item|
+      image_tag item.sprit, alt: "#{item.name}_sprit"
+    end
+    column :category_id
+    column :upload do |item|
+      image_tag item.upload, alt: "#{item.name}_upload" if item.upload.present?
+    end
+  end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -12,4 +12,5 @@ class Item < ApplicationRecord
 
   has_many :orders
   has_many :users, through: :orders
+  has_one_attached :upload
 end
