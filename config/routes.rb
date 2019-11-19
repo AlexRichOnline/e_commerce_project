e495c6519dd906e52e71e0f88e4d4d753f116699
Alex Richardson <arichardson110@academic.rrc.ca>
2019-11-19 12:02:52 -0600

    attmepting to revert a series of changes to be before the clearance gem was added

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,25 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  resources :passwords, controller: 'clearance/passwords', only: %i[create new]
+  resource :session, controller: 'clearance/sessions', only: [:create]
+
+  resources :users, controller: 'clearance/users', only: [:create] do
+    resource :password,
+             controller: 'clearance/passwords',
+             only: %i[create edit update]
+  end
+
+  get '/log_in' => 'clearance/sessions#new', as: 'log_in'
+  delete '/log_out' => 'clearance/sessions#destroy', as: 'log_out'
+  get '/register' => 'clearance/users#new', as: 'register'
+
   get 'pages/:permalink', to: 'pages#permalink', as: 'permalink'
   get 'pages/about_us', to: 'pages#permalink', as: 'about_us'
+
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
+
   resources :categories, only: %i[index show]
   resources :items, only: %i[index show] do
     collection do
