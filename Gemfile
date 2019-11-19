cbd57559a6df6cc6d12163697652bbdca9371bfc
Alex <arichardson110@academic.rrc.ca>
2019-11-19 12:44:03 -0600

    attempt to fix revert issues

--- Gemfile
+++ Gemfile
@@ -31,7 +31,6 @@ gem 'jbuilder', '~> 2.7'
 gem 'bootsnap', '>= 1.4.2', require: false
 
 gem 'activeadmin'
-gem 'clearance'
 gem 'devise'
 gem 'kaminari'
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_11_190015) do
+ActiveRecord::Schema.define(version: 2019_11_06_140204) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -119,16 +119,6 @@ ActiveRecord::Schema.define(version: 2019_11_11_190015) do
     t.string "password"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "email"
-    t.string "encrypted_password", limit: 128
-    t.string "confirmation_token", limit: 128
-    t.string "remember_token", limit: 128
-    t.integer "province_id"
-    t.string "postal_code"
-    t.string "address"
-    t.index ["email"], name: "index_users_on_email"
-    t.index ["province_id"], name: "index_users_on_province_id"
-    t.index ["remember_token"], name: "index_users_on_remember_token"
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
